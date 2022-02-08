32a0d8b7a335ca447eb87b863b4e0dd2f648e0c8
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-02-08 09:32:52 +0530

    added configs

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -11,7 +11,11 @@ export default defineNuxtConfig({
     firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET || "XXXXXXXXXXXXXXXXX",
     firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "XXXXXXXXXXXXXXXXX",
     firebaseAppId: process.env.FIREBASE_APP_ID || "XXXXXXXXXXXXXXXXX",
-    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID || "XXXXXXXXXXXXXXXXX"
+    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID || "XXXXXXXXXXXXXXXXX",
+    instagramClientId: process.env.INSTAGRAM_CLIENT_ID || "XXXXXXXXXXXXXXXXX",
+    instagramRedirectUri: process.env.INSTAGRAM_REDIRECT_URI || "https://localhost:3000/auth/instagram-verify/",
+    instagramVerifyUrl: process.env.FIREBASE_FUNCTIONS_INSTAGRAM || "http://localhost:5001/incameo-test/us-central1/api/instagramVerify",
+
   },
     vite: {
         server: {
